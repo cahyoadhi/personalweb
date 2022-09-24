a53133d7171bfb4747601905c9e123986383d7ca
cahyoadhi <90748704+cahyoadhi@users.noreply.github.com>
2022-09-24 22:12:01 +0700

    main

--- personalweb/models.py
+++ personalweb/models.py
@@ -1,3 +1,11 @@
+from pyexpat.errors import messages
 from django.db import models
 
 # Create your models here.
+class Email(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    subject = models.CharField(max_length=50, null=True, blank=True)
+    message = models.TextField()
+
+    def __str__(self):
+        return str(self.name) 
--- personalweb/views.py
+++ personalweb/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from django.contrib import messages
+from .models import Email
 # Create your views here.
 
 def homePage(request):
+    if request.method=='POST':
+        newemail = Email()
+        newemail.name = request.POST.get('name')
+        newemail.subject = request.POST.get('subject')
+        newemail.message = request.POST.get('message')
+        newemail.save()
+        messages.success(request, "Email sent")
     context = {}
     return render (request, 'index.html', context)
